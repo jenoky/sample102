0f31999c40a5a94d7d52f6b7c102a5e60f0e1789
Jeno KQY <78352833+jenoky@users.noreply.github.com>
2023-01-03 15:31:46 +0530

    trigger 23

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
 app.listen(3001, () => {
- console.log("Server running on port 3000");
+ console.log("Server Launched");
 });
 app.get("/url", (req, res, next) => {
- res.json(["Successfully launched NM101 Project Code"]);
+ res.json(["Successfully launched Project Code"]);
 });
 app.get("/ready", (req, res, next) => {
  res.sendStatus(200);
 });
-// added for trigger 6
+// added for trigger 23
